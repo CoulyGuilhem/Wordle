929e985aa1260fbddf32533419fc8eb612c639d5
Couly Guilhem <couly.guilhem@gmail.com>
2022-02-08 23:19:04 +0100

    game board

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {SettingsService} from "../settings.service";
 
@@ -7,24 +7,35 @@ import {SettingsService} from "../settings.service";
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
 
   wordLength: number;
   tries: number;
   settings: number[];
+  board = new Array();
   word = new Array();
   load = false;
 
   constructor(public router: Router, public serviceSettings: SettingsService) {
   }
 
+  ionViewDidEnter(){
+    this.loadSettings();
+  }
+
   loadSettings(){
+    this.board = new Array();
+    this.word = new Array();
     this.settings = this.serviceSettings.getSettings();
     this.wordLength = this.settings[0];
     this.tries = this.settings[1];
-    for(let i = 0;i<this.wordLength;i++){
-      this.word.push(' ');
-      console.log(i);
+    for(let j = 0;j<this.tries;j++){
+      for(let i = 0;i<this.wordLength;i++){
+        this.word.push(' ');
+        console.log(i);
+      }
+      this.board.push(this.word);
+      this.word = new Array();
     }
   }
 
