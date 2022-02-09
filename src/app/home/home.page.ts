f56c84ea1891e40178b2fb7a41573baee30072fe
CoulyGuilhem <couly.guilhem@gmail.com>
2022-02-09 15:57:36 +0100

    start merging

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,15 +11,45 @@ import {SettingsService} from '../settings.service';
 })
 export class HomePage{
 
-
   wordLength: number;
   tries: number;
+  indexWord: number;
+  indexLetter: number;
   settings: number[];
   board = new Array();
   word = new Array();
   load = false;
+  show: boolean = true;
+
+  keyboard: Array<Array<string>> = [['a','z','e','r','t','y','u','i','o','p'],
+                                    ['q','s','d','f','g','h','j','k','l','m'],
+                                    ['w','x','c','v','b','n','<--','Enter']];
 
   constructor(public router: Router, public serviceSettings: SettingsService) {
+  }
+
+  reload() {
+    this.show = false;
+    setTimeout(() => this.show = true);
+  }
+  checkMot(){
+
+  }
+  deleteLetter(){
+    if(this.indexLetter > 0){
+      this.board[this.indexWord][this.indexLetter -1] = '';
+      this.indexLetter = this.indexLetter -1;
+    }
+  }
+
+  addLetter(letter: string){
+    console.log("Letter : "+letter);
+    if(this.indexLetter < this.board[this.indexWord].length){
+      this.board[this.indexWord][this.indexLetter] = letter;
+      this.indexLetter = this.indexLetter +1;
+      console.log(this.word.toString());
+    }
+    this.reload();
   }
 
   ionViewDidEnter(){
@@ -27,6 +57,8 @@ export class HomePage{
   }
 
   loadSettings(){
+    this.indexWord = 0;
+    this.indexLetter = 0;
     this.board = new Array();
     this.word = new Array();
     this.settings = this.serviceSettings.getSettings();
@@ -35,7 +67,6 @@ export class HomePage{
     for(let j = 0;j<this.tries;j++){
       for(let i = 0;i<this.wordLength;i++){
         this.word.push(' ');
-        console.log(i);
       }
       this.board.push(this.word);
       this.word = new Array();
